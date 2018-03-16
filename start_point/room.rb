1dc4a748a3a139a7ba19e1b3df0f58c2d62c26bf
Jaime Lopez <detec.azul@gmail.com>
2018-03-16 11:47:53 +0000

    MVP completed

--- start_point/room.rb
+++ start_point/room.rb
@@ -7,4 +7,31 @@ class Room
     @occupants = []
   end
 
+  def add_guest_to_occupants(guest)
+    @occupants << guest
+  end
+
+  def find_occupant_by_name(occupant_name)
+    return @occupants.find { |occupant| occupant.name == occupant_name }
+  end
+
+  def remove_guest_from_occupants(guest)
+    guest = find_occupant_by_name(guest) if guest.class == String
+    @occupants.delete(guest)
+  end
+
+  def add_song_to_playlist(song)
+    @playlist << song
+  end
+
+  def find_song_by_title(song_title)
+    return @playlist.find { |song| song.title == song_title }
+  end
+
+  def remove_song_from_playlist(song)
+    song = find_song_by_title(song) if song.class == String
+    @playlist.delete(song)
+  end
+
+
 end
--- start_point/specs/room_spec.rb
+++ start_point/specs/room_spec.rb
@@ -14,9 +14,16 @@ class TestRoom < MiniTest::Test
     song05 = Song.new("A Whole New World")
     song06 = Song.new("A Friend Like Me")
 
+    @song07 = Song.new("Mazinger Z")
+
     rock_playlist = [song01, song02]
     lady_gaga_playlist = [song03, song04]
     disney_playlist = [song05, song06]
+
+    @guest01 = Guest.new("Jaime")
+    @guest02 = Guest.new("Ceci")
+    @guest03 = Guest.new("Laia")
+    @guest04 = Guest.new("Rubén")
 
     @disney_room = Room.new(1, disney_playlist)
     @rock_room = Room.new(2, rock_playlist)
@@ -33,6 +40,57 @@ class TestRoom < MiniTest::Test
 
   def test_get_room_play_list()
     assert_equal("A Friend Like Me", @disney_room.playlist.last.title())
+  end
+
+  def test_add_guest_to_occupants()
+    @disney_room.add_guest_to_occupants(@guest01)
+    @disney_room.add_guest_to_occupants(@guest02)
+    assert_equal(2, @disney_room.occupants.count())
+  end
+
+  def test_remove_guest_from_occupants()
+    @disney_room.add_guest_to_occupants(@guest01)
+    @disney_room.add_guest_to_occupants(@guest02)
+    @disney_room.remove_guest_from_occupants(@guest01)
+    assert_equal("Ceci", @disney_room.occupants.last.name())
+    assert_equal(1, @disney_room.occupants.count())
+  end
+
+  def test_find_occupant_by_name()
+    @disney_room.add_guest_to_occupants(@guest01)
+    @disney_room.add_guest_to_occupants(@guest02)
+    guest = @disney_room.find_occupant_by_name("Jaime")
+    @disney_room.remove_guest_from_occupants(guest)
+    assert_equal("Ceci", @disney_room.occupants.last.name())
+    assert_equal(1, @disney_room.occupants.count())
+  end
+
+  def test_remove_guest_from_occupants__using_string_name()
+    @disney_room.add_guest_to_occupants(@guest01)
+    @disney_room.add_guest_to_occupants(@guest02)
+    @disney_room.remove_guest_from_occupants("Jaime")
+    assert_equal("Ceci", @disney_room.occupants.last.name())
+  end
+
+  def test_add_song_to_playlist()
+    @disney_room.add_song_to_playlist(@song07)
+    assert_equal(3, @disney_room.playlist.count())
+    assert_equal("Mazinger Z", @disney_room.playlist.last.title())
+  end
+
+  def test_remove_song_from_playlist()
+    @disney_room.remove_song_from_playlist(@disney_room.playlist.first)
+    assert_equal(1, @disney_room.playlist.count())
+  end
+
+  def test_find_song_by_title()
+    result = @disney_room.find_song_by_title("A Whole New World")
+    assert_equal("A Whole New World", result.title)
+  end
+
+  def test_remove_song_from_playlist__using_string_title()
+    @disney_room.remove_song_from_playlist("A Whole New World")
+    assert_equal(1, @disney_room.playlist.count())
   end
 
 end
